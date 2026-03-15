d44d77e2fdb3045311c185b55b12b1e39d0cbd14
Palmer Maurer <psm76@georgetown.edu>
2026-03-15 19:09:46 -0400

    Just some changes to `Build_and_Deploy.sh`!

--- Build_and_Deploy.sh
+++ Build_and_Deploy.sh
@@ -1,5 +1,5 @@
 # GitHub sync!
-./git.sh
+./Git.sh
 
 # Clean up the old website build!
 rm -rf _site
@@ -12,9 +12,8 @@ for i in $(find _site -type f); do chmod 644 $i; done
 for i in $(find _site -type d); do chmod 755 $i; done
 
 # Push the site to the remote server: Georgetown Domains!
-read -p "Would you like to push to Georgetown Domains? (y/n): " 
-read answer
-if [ "$answer" != "${answer#[Yy]}" ]; then 
+read -p "Would you like to push to Georgetown Domains? (y/n): " answer
+if [[ "$answer" =~ ^[Yy]$ ]]; then
     rsync -av --delete _site/ pmaurerg@pmaurer.georgetown.domains:~/public_html/
     echo "Deployed successfully!"
 else
