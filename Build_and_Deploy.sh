121e836219bacaf32c0c78796f6403202507cea3
Palmer Maurer <psm76@georgetown.edu>
2026-03-15 18:55:38 -0400

    A couple minor changes!

--- Build_and_Deploy.sh
+++ Build_and_Deploy.sh
@@ -12,7 +12,7 @@ for i in $(find _site -type f); do chmod 644 $i; done
 for i in $(find _site -type d); do chmod 755 $i; done
 
 # Push the site to the remote server: Georgetown Domains!
-read -p "Would you like to push to Georgetown Domains? (y/n):" 
+read -p "Would you like to push to Georgetown Domains? (y/n): " 
 read answer
 if [ "$answer" != "${answer#[Yy]}" ]; then 
     rsync -av --delete _site/ pmaurerg@pmaurer.georgetown.domains:~/public_html/
